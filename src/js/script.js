806936289c29fc1d64da91af205da70ef82f4e36
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-11-09 19:56:45 +0300

    middleBlock

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 const firstBtn = document.querySelector('#firstBtn'),
     secondBtn = document.querySelector('#secondBtn'),
     firstBlock = document.querySelector('.col-12'),
-    leftBlock = document.querySelector(".block_middle"),
+    middleBlock = document.querySelector('.block_middle'),
     modal = document.querySelector('.modal'),
     btnClose = document.querySelector('.btn-close');
 
@@ -11,7 +11,7 @@ const toggleElem = (elem, classActive) => {
 
 firstBtn.addEventListener('click', () => toggleElem(firstBlock, 'hide'));
 btnClose.addEventListener('click', () => toggleElem(modal, 'hide'));
-secondBtn.addEventListener("click", () => toggleElem(leftBlock, 'order'));
+secondBtn.addEventListener("click", () => toggleElem(middleBlock, 'order'));
 
 
  
